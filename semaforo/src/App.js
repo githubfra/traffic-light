9c3926e40fdf6e3b4be77593c807552a7436d2da
githubfra <diazcabellofrancisca@gmail.com>
2022-09-15 00:30:34 +0000

    First commit

---
+++ semaforo/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import React, { useState } from "react";
+
+
+  export function App() {
+    const [selectedColor, setSelectedColor] = useState("yellow");
+
+      return (
+          <div className="traffic-light">
+              <div 
+                  onClick={() => setSelectedColor("red")}
+                  className={
+                    "light red" + (selectedColor == "red" ? " glow" : "")
+                    }></div>
+              <div 
+                  onClick={() => setSelectedColor("yellow")}
+                  className={
+                    "light yellow" + (selectedColor == "yellow" ? " glow" : "")
+                    }></div>
+              <div 
+                  onClick={() => setSelectedColor("green")}
+                  className={
+                    "light green" + (selectedColor == "green" ? " glow" : "")
+                    }></div>            
+                  
+          </div>
+      );
+  }
+
+export default App;
